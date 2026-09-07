eff2637807b6f47e5ddefc4349217c22d8421e74
wassim7254 <wwassim.wassim2009@gmail.com>
2026-09-07 02:33:33 +0100

    add PWA files

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'todo-pro-v1'
+
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request)
+    }),
+  )
+})
